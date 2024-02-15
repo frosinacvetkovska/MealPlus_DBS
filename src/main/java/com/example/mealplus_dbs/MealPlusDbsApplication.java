19a948d0c2f4ab5ad651d2a199d84836a7646b4e
frosinacvetkovska <frosinacvetkovska29@gmail.com>
2024-02-15 18:52:12 +0100

    Initial creation

---
+++ src/main/java/com/example/mealplus_dbs/MealPlusDbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealplus_dbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlusDbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealPlusDbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mealplus_dbs/MealPlusDbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealplus_dbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlusDbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
